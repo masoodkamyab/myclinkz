d9479a46e019266eafebb19bf2a32cccc65e5ef2
masoodkamyab <masoodkamyab@users.noreply.github.com>
2022-12-03 07:54:37 +0000

    ✅ Ready to clone and code.

--- myclinkz/config.py
+++ myclinkz/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="myclinkz",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="myclinkz_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from myclinkz.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export myclinkz_KEY=value
+export myclinkz_KEY="@int 42"
+export myclinkz_KEY="@jinja {{ this.db.uri }}"
+export myclinkz_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+myclinkz_ENV=production myclinkz run
 ```
 
 Read more on https://dynaconf.com
